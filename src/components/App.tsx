0babd6c22b5ca48426946d843c2418c2b67a8ceb
Maria G <ruiined@gmail.com>
2023-03-07 13:35:17 +0000

    feat: setup tabs and table

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,30 @@
 import { Tab } from "@headlessui/react";
 
+const CATEGORIES = [{ title: "Images" }, { title: "Predictions" }];
+
 const App = () => {
   return (
     <div>
       <h1>Image Recognition</h1>
       <Tab.Group>
         <Tab.List>
-          <Tab>Images</Tab>
-          <Tab>Predictions</Tab>
+          {CATEGORIES.map(({ title }) => (
+            <Tab key={title}>{title}</Tab>
+          ))}
         </Tab.List>
         <Tab.Panels>
-          <Tab.Panel>Image table</Tab.Panel>
+          <Tab.Panel>
+            <table className="table-auto">
+              <thead>
+                <tr>Name</tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Value</td>
+                </tr>
+              </tbody>
+            </table>
+          </Tab.Panel>
           <Tab.Panel>Prediction table</Tab.Panel>
         </Tab.Panels>
       </Tab.Group>
